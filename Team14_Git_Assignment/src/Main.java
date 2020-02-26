4051d2c441e2874cf1e64b01f1251eb06f4a3853
mona010 <36086043+mona010@users.noreply.github.com>
2020-02-26 20:35:51 +0200

    Update Main.java

--- Team14_Git_Assignment/src/Main.java
+++ Team14_Git_Assignment/src/Main.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 public class Main {
 	private static ISubscriber subscribers [] = {
-			new SimpleSubscriber(),
-            new ReallySimpleSubscriber(),
-            new CircleArea(),
-            new Fibonacci() , 
-	};
+			                             new SimpleSubscriber(),
+                                                     new ReallySimpleSubscriber(),
+                                                     new CircleArea(),
+                                                     new Fibonacci() ,
+	                                             new CircleVolum(),
+	                                            };
+	
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
 		for (ISubscriber sub : subscribers) {
