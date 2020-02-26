4b244d2cfde87ddf289bad80ac1c7c395d59ab12
mona010 <36086043+mona010@users.noreply.github.com>
2020-02-26 20:16:43 +0200

    Update CircleVolum.java

--- Team14_Git_Assignment/src/CircleVolum.java
+++ Team14_Git_Assignment/src/CircleVolum.java
@@ -1,7 +1,6 @@
 public class CircleVolum implements ISubscriber{
     @Override
     public void notifySubscriber(String input) {
-        // TODO Auto-generated method stub
         double pi = 22/7.0 ,volume = 0.0 , c = 4/3.0;
         volume = c * pi * Math.pow( Double.parseDouble(input) , 3);
         System.out.println("CircleVolume = " + volume);
