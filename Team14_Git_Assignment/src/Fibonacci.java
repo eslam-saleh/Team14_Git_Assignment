4b6c11b1ced8f8bd16ba1686a102f9b987b28565
alaafar256 <58630587+alaafar256@users.noreply.github.com>
2020-02-28 20:05:07 +0200

    Add files via upload

---
+++ Team14_Git_Assignment/src/Fibonacci.java
@@ -0,0 +1,26 @@
+public class Fibonacci implements ISubscriber {
+
+	public static int Fibo(int n )
+	{
+     if (n==0)
+    	 {return 0 ;}  
+     if (n==1 || n==2 )
+    	 { 
+    	 return 1 ; 
+    	 } 
+    	 return Fibo(n-2) + Fibo(n-1) ; 
+		
+	}
+	@Override
+	public void notifySubscriber(String input) {
+		
+		int numberofterms = Integer.parseInt(input);
+		for (int i = 0 ; i <numberofterms ; i++ )
+		{
+			System.out.print(Fibo(i) + "\n");
+		}
+		
+	}
+
+
+}
