189025f83670b2a0cf89899ae2f337c42e822e9e
ahmedhablas <36382912+ahmedhablas@users.noreply.github.com>
2020-02-28 23:23:59 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	private static ISubscriber subscribers [] = {
+			                             new SimpleSubscriber(),
+						     new SphereCircumference(),
+                                                     new ReallySimpleSubscriber(),
+                                                     new CircleArea(),
+                                                     new Fibonacci() ,
+	                                             new CircleVolum() ,
+						     new SphereVolume()
+	                                            };
+	
+	public static void main(String[] args) {
+		Topic mathTopic = new Topic();
+		for (ISubscriber sub : subscribers) {
+			mathTopic.addSubscriber(sub);
+		}
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		mathTopic.dispatchEvent(input);
+	}
+}
