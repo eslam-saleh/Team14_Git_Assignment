3a44f0f7dac11b3070925ce31d2306f0f49c24d0
ahmedhablas <36382912+ahmedhablas@users.noreply.github.com>
2020-02-28 23:28:45 +0200

    Add files via upload

--- git_assignment_initial-master/src/Main.java
+++ git_assignment_initial-master/src/Main.java
@@ -2,10 +2,15 @@ import java.util.Scanner;
 
 public class Main {
 	private static ISubscriber subscribers [] = {
-			new SimpleSubscriber(),
-			new ReallySimpleSubscriber(),
-			new sphereCirc(),
-	};
+			                             new SimpleSubscriber(),
+						     new SphereCircumference(),
+                                                     new ReallySimpleSubscriber(),
+                                                     new CircleArea(),
+                                                     new Fibonacci() ,
+	                                             new CircleVolum() ,
+						     new SphereVolume()
+	                                            };
+	
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
 		for (ISubscriber sub : subscribers) {
