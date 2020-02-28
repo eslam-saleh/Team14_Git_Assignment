0672e45f63084554283658ec8168f81fe9a44da4
Maria <mariaemil55@gmail.com>
2020-02-28 22:30:56 +0200

    Added 2PowerN class
    A class that calculates any 2^N equation.

--- Team14_Git_Assignment/src/Main.java
+++ Team14_Git_Assignment/src/Main.java
@@ -9,6 +9,7 @@ public class Main {
                                                      new Fibonacci() ,
 	                                             new CircleVolum(),
 						     new SummationSeries(),
+			new TwoPowerN();
 	                                            };
 	
 	public static void main(String[] args) {
