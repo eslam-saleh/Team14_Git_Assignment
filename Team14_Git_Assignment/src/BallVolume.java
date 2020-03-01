86d89bb8e17f87356fdcca21a22bef72d6a1c867
mona010 <36086043+mona010@users.noreply.github.com>
2020-03-01 14:42:22 +0200

    20170300 -> update

--- Team14_Git_Assignment/src/BallVolume.java
+++ Team14_Git_Assignment/src/BallVolume.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package git_assignment;
-
-/**
- *
- * @author DELL
- */
 public class BallVolume implements ISubscriber{
     public double ball_volume(String r){
         double pi = 22/7.0 ,volume = 0.0 , c = 4/3.0;
