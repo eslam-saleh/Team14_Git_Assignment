dbfebcbef6250fe3445c9c031660d437fb1aa526
mohammedsaad9843 <60687940+mohammedsaad9843@users.noreply.github.com>
2020-03-02 16:45:43 -0500

    Add files via upload

---
+++ Team14_Git_Assignment/src/lucasSeries.java
@@ -0,0 +1,16 @@
+
+public class lucasSeries implements ISubscriber {
+    public static int Fibo(int n )
+    {
+        if(n==0) {return 2;}
+        if(n==1) {return 1;}
+        return Fibo(n-2) + Fibo(n-1) ;
+    }
+
+    @Override
+    public void notifySubscriber(String input) {
+        int numberofterms = Integer.parseInt(input);
+        for(int i = 0 ; i <numberofterms ; i++)
+            System.out.print(Fibo(i) + " ");
+    }
+}
