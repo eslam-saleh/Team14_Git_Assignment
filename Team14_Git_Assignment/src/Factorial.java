d40d1867a792c883856b0f5072f0c00c8c9c0d85
Mahmoud-azzam993 <54447534+Mahmoud-azzam993@users.noreply.github.com>
2020-03-03 01:42:19 +0200

    Add files via upload

---
+++ Team14_Git_Assignment/src/Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial {
+    public Factorial(){
+
+    }
+    
+
+    public int calculateFactorialWithLoop(int number){
+        int result = 1;
+        for (int i = number ; i>1 ; i--){
+            result = result * i;
+        }
+        return result;
+    }
+}
