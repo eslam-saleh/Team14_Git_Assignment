88a82bb6e3dba3b3fc12ac08e9187f5c41a7718b
mohammedsaad9843 <60687940+mohammedsaad9843@users.noreply.github.com>
2020-03-02 16:48:45 -0500

    Add files via upload

--- Team14_Git_Assignment/src/Main.java
+++ Team14_Git_Assignment/src/Main.java
@@ -2,20 +2,16 @@ import java.util.Scanner;
 
 public class Main {
 	private static ISubscriber subscribers [] = {
-			                             new SimpleSubscriber(),
-						     new SphereCircumference(),
+			                                         new SimpleSubscriber(),
+												     new SphereCircumference(),
                                                      new ReallySimpleSubscriber(),
                                                      new CircleArea(),
                                                      new Fibonacci() ,
-	                                             new BallVolume(),
-						     new SummationSeries(),
-
-			new TwoPowerrN();
-
-						   
-						     new SphereVolume()
-
-	                                            };
+													 new BallVolume(),
+						     						 new SummationSeries(),
+													 new TwoPowerrN(),
+													 new SphereVolume(),
+													 new lucasSeries()};
 	
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
